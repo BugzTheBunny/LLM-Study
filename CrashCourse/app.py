a09faf655dd4a58eb17ea86ca81ea5b34f83d877
BugzTheBunny <urigami2010@gmail.com>
2024-01-28 09:35:43 +0200

    created first 'chatbot' for Q&A, and deployed it on hugging face

--- CrashCourse/app.py
+++ CrashCourse/app.py
@@ -1,11 +1,39 @@
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.schema import BaseOutputParser
 from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
 from dotenv import load_dotenv
 import streamlit as st
+import os
 
-
+st.set_page_config(page_title="QA Bot")
+MODEL = "gpt-3.5-turbo-instruct"
 load_dotenv()
 
-def get_openai_response(question:str):
-    pass
+
+def get_api_key(key: str) -> str:
+    return os.getenv(key)
+
+
+def get_openai_response(question: str):
+    if question:
+        llm = OpenAI(
+            openai_api_key=get_api_key("OPENAI_API_KEY"),
+            model=MODEL,
+            temperature=0.5
+        )
+        response = llm(question)
+        return response
+
+
+user_input = st.text_input(label="Question", key="user_input")
+
+llm_response = get_openai_response(user_input)
+
+st.header("LangChain Bot")
+
+submit = st.button("Ask")
+
+if submit:
+    st.subheader("Response:")
+    st.write(llm_response)
--- README.md
+++ README.md
@@ -3,8 +3,11 @@ Following [Krish Naik](https://www.youtube.com/channel/UCNU_lfiiWBdtULKOw6X0Dig)
 
 - Using `jupyter-lab` for experimenting
 
-1. `CrashCourse` - The basics of using LLM
-    - **.env content**:  
-   
-      ` OPENAI_API_KEY="Your API Key"`  
-      `HUGGINGFACEHUB_API_TOKEN="Your API Key"`
+1. `CrashCourse / Basic Q&A Bot` - The basics of using LLM  
+    - **Summery**:  
+    `Created a basic Q&A bot using GPT-3.5 Turbo`
+      - **.env content**:  
+
+        `OPENAI_API_KEY="Your API Key"`  
+        `HUGGINGFACEHUB_API_TOKEN="Your API Key"`
+      - [Link for App](https://huggingface.co/spaces/BugzTheBunny1/LangChainBasicQABot)
